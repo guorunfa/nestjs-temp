83f8ae8ad5dd4a0951b8feb52d61302e33a55791
guorunfa <guorunfa@aliyun.com>
2023-03-08 18:14:15 +0800

    dotenv

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log('cc', this.configService.get('DB'));
     return this.appService.getHello();
   }
 }
