e41807c6fab5787f9392d5e8fe1104833ff1957d
guorunfa <guorunfa@aliyun.com>
2023-05-16 18:13:18 +0800

    update

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -3,16 +3,18 @@ import { Profile } from './profile.entity';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn() // 主键
   id: number;
 
-  @Column()
+  @Column() // 字段
   username: string;
 
   @Column()
   password: string;
   logs: any;
 
+  @Column()
+  age: number;
   // 关联表，(profile) => profile.user关联表里的字段
   @OneToOne(() => Profile, (profile) => profile.user)
   profile;
